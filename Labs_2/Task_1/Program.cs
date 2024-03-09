97cf9ec10396ea87a3bc56cf80679b369d1c4d3f
Pawel <s27482@pjwstk.edu.pl>
2024-03-10 00:56:36 +0100

    Add average feature

--- Labs_2/Task_1/Program.cs
+++ Labs_2/Task_1/Program.cs
@@ -3,3 +3,11 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("1st Modification");
 Console.WriteLine("2nd Modification");
 Console.WriteLine("3rd Modification");
+
+static double average(int[] array)
+{
+    var sum = 0;
+    foreach(var element in array)
+        sum += element;
+    return (double)sum / array.Length;
+}
